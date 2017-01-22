e158da73378ad4b701c67496923c257cfcd3a60c
thulsamrala <thulsamrala.samy.hcl@gmail.com>
2017-01-21 21:31:21 -0700

    Build...

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.learntek.devops.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
