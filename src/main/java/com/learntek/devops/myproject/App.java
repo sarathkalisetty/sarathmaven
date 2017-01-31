5bb42a04abdd03de48efef300ef5541019e061af
thulsamrala <thulsamrala.samy.hcl@gmail.com>
2017-01-30 19:35:54 -0700

    sprint1 branch changes..

--- src/main/java/com/learntek/devops/myproject/App.java
+++ src/main/java/com/learntek/devops/myproject/App.java
@@ -12,5 +12,6 @@ public class App
 System.out.println( "Hello DevOpsWorld!" );
 System.out.println( "Hello DevOpsWorld Diffrence!" );
 System.out.println( "Hello DevOpsWorld Develop branch!" );
-    }
+
+System.out.println( "Hello DevOpsWorld sprint1 branch!" );    }
 }
